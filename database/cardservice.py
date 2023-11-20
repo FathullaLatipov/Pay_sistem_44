5515473ee727bbb005c7aa66c14d0c59a261a69c
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-11-20 17:05:30 +0500

    добавили дз

---
+++ database/cardservice.py
@@ -0,0 +1,6 @@
+#ДЗ!!!
+# Добавления карты def add_card_db(user_id, card_number, balance, card_name, exp_date)
+# Удаления карты def_delete_card_db()
+# Вывести все карты определенного пользователя пользователя get_exact_user_cards_db()
+# Вывксти определенную карту def get_exact_card_db()
+# Проверка карты на наличие в базе то есть есть ли такая карта def check_card_info_db(
